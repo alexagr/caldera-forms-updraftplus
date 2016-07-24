71e1a25753c16e3f901b8a0e0dc7b419bcd477c3
Alex Agranov <alex.agranov@gmail.com>
2016-07-24 18:06:32 +0300

    Individual folder for each form + index file

--- cf-updraft.php
+++ cf-updraft.php
@@ -15,18 +15,18 @@ add_filter('caldera_forms_get_form_processors', 'cf_updraft_register_processor')
 add_filter('updraft_backupable_file_entities_final', 'cf_updraft_add_entries');
         
 function cf_updraft_register_processor($pr) {
-	$pr['updraft'] = array(
-		"name"              =>  __('UpdraftPlus', 'cf-updraft'),
-		"description"       =>  __("Save file on submission", 'cf-updraft'),
-		"author"            =>  'Alex Agranov',
-		"icon"				=>	plugin_dir_url(__FILE__) . "icon.png",
-		"processor"         =>  'cf_updraft_save_file',
-		"template"          =>  plugin_dir_path(__FILE__) . "config.php",
-	);
+    $pr['updraft'] = array(
+        "name"              =>  __('UpdraftPlus', 'cf-updraft'),
+        "description"       =>  __("Save file on submission", 'cf-updraft'),
+        "author"            =>  'Alex Agranov',
+        "icon"              =>  plugin_dir_url(__FILE__) . "icon.png",
+        "processor"         =>  'cf_updraft_save_file',
+        "template"          =>  plugin_dir_path(__FILE__) . "config.php",
+    );
     return $pr;
 }         
          
-function cf_updraft_save_file($config, $form) {
+function cf_updraft_create_file($config, $form, $filename, $dir, $date) {
     // create message
     $message = $form['mailer']['email_message']; 
     if(empty($message)) {
@@ -83,32 +83,91 @@ function cf_updraft_save_file($config, $form) {
         }
     }
 
-    $filename = trim($config['filename']); 
-    if(empty($filename)) {
-        $filename = "Form";
-    }
-    $filename = Caldera_Forms::do_magic_tags($filename); 
-
     // write file
-    $log_directory = WP_CONTENT_DIR.'/uploads/caldera-forms/';
-    if (!is_dir($log_directory)) {
-        //try to make a directory and create an .htaccess file
-    	if( @mkdir($log_directory, 0755) ){
-    	    $file = @fopen($log_directory.'.htaccess','w');
-		    if( $file ){
-				fwrite($file, 'deny from all');
-				fclose($file);
-		    }
-    	}
-    }
-    $file = @fopen( $log_directory.$filename.'_'.date('Y-m-d H:i:s').'.html' ,'a+');
+    $file = @fopen( $dir . $filename . '_' . str_replace(' ', '_', $date) . '.html' ,'a');
     if( $file ){
         fwrite($file, $message);
         fclose($file);
     }
 }
 
+
+function cf_updraft_update_index($config, $form, $filename, $dir, $date) {
+    $title = trim($config['title']); 
+    if (empty($title)) {
+        $title = $filename;
+    }
+    $title = Caldera_Forms::do_magic_tags($title); 
+
+    // update body.html
+    $file = @fopen($dir . 'body.html', 'a');
+    if ($file) {
+        fwrite($file, "  <tr valign=\"top\">\n");
+        fwrite($file, "    <td nowrap=\"nowrap\">" . $date . "</td>\n");
+        fwrite($file, "    <td width=\"90%\"><a href=\"" . $filename . "_" . str_replace(' ', '_', $date) . ".html\">" . $title . "</a></td>\n");
+        fwrite($file, "  </tr>\n");
+    }
+    
+    // create index.html
+    $out = @fopen($dir . 'index.html', 'w');
+    $in = @fopen(plugin_dir_path( __FILE__ ) . 'header.html', 'r');
+    if ($in) {
+        while (($line = fgets($in)) !== false) {
+            fwrite($out, str_replace('{form_name}', $form['name'], $line));
+        }
+        fclose($in);
+    }
+    $in = @fopen($dir . 'body.html', 'r');
+    if ($in) {
+        while (($line = fgets($in)) !== false) {
+            fwrite($out, $line);
+        }
+        fclose($in);
+    }
+    $in = @fopen(plugin_dir_path( __FILE__ ) . 'footer.html', 'r');
+    if ($in) {
+        while (($line = fgets($in)) !== false) {
+            fwrite($out, $line);
+        }
+        fclose($in);
+    }
+}
+
+
+function cf_updraft_save_file($config, $form) {
+    $directory = trim($config['directory']); 
+    $filename = trim($config['filename']); 
+    if (empty($filename)) {
+        $filename = "Form";
+    }
+    $filename = Caldera_Forms::do_magic_tags($filename);
+    $filename = str_replace(' ', '%20', $filename); 
+
+    $dir = WP_CONTENT_DIR . '/uploads/caldera-forms/';
+    if (!is_dir($dir)) {
+        @mkdir($dir, 0755);
+          $file = @fopen($log_directory.'.htaccess','w');
+            if ($file)  {
+            fwrite($file, 'deny from all');
+            fclose($file);
+            }
+    }
+
+    if (!empty($directory)) {
+        $dir = $dir . $directory . '/'; 
+    }
+    if (!is_dir($dir)) {
+        @mkdir($dir, 0755);
+    }
+
+    $date = date('Y-m-d H:i:s');
+    
+    cf_updraft_create_file($config, $form, $filename, $dir, $date);
+    cf_updraft_update_index($config, $form, $filename, $dir, $date);
+}
+
+
 function cf_updraft_add_entries($arr, $full_info = false) {
-	$arr['forms'] = array('path' => WP_CONTENT_DIR . '/uploads/caldera-forms' , 'description' => __('Caldera forms', 'cf-updraft'));
+    $arr['forms'] = array('path' => WP_CONTENT_DIR . '/uploads/caldera-forms' , 'description' => __('Caldera forms', 'cf-updraft'));
     return $arr;
 }
--- config.php
+++ config.php
@@ -1,20 +1,36 @@
 <div class="caldera-config-group">
-	<label><?php echo __('Filename', 'cf-updraft'); ?> </label>
-	<div class="caldera-config-field">
-		<input type="text" class="block-input field-config required" name="{{_name}}[filename]" value="{{filename}}">
-	</div>
+    <label><?php echo __('Filename', 'cf-updraft'); ?> </label>
+    <div class="caldera-config-field">
+        <input type="text" class="block-input field-config required" name="{{_name}}[filename]" value="{{filename}}">
+    </div>
 </div>
 
 <div class="caldera-config-group">
-	<label><?php echo __('Header', 'cf-updraft'); ?> </label>
-	<div class="caldera-config-field">
-		<textarea class="block-input field-config required magic-tag-enabled" name="{{_name}}[header]">{{#if header}}{{header}}{{else}}<?php _e("<html>\r\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\r\n<body>", 'cf-updraft'); ?>{{/if}}</textarea>
-	</div>
+    <label><?php echo __('Directory', 'cf-updraft'); ?> </label>
+    <div class="caldera-config-field">
+        <input type="text" class="block-input field-config required" name="{{_name}}[directory]" value="{{directory}}">
+    </div>
 </div>
 
 <div class="caldera-config-group">
-	<label><?php echo __('Footer', 'cf-updraft'); ?> </label>
-	<div class="caldera-config-field">
-		<textarea class="block-input field-config required magic-tag-enabled" name="{{_name}}[footer]">{{#if footer}}{{footer}}{{else}}<?php _e("</body>\r\n</html>", 'cf-updraft'); ?>{{/if}}</textarea>
-	</div>
+    <label><?php echo __('Title', 'cf-updraft'); ?> </label>
+    <div class="caldera-config-field">
+        <input type="text" class="block-input field-config required" name="{{_name}}[title]" value="{{title}}">
+    </div>
+</div>
+
+<hr /> 
+
+<div class="caldera-config-group">
+    <label><?php echo __('Header', 'cf-updraft'); ?> </label>
+    <div class="caldera-config-field">
+        <textarea class="block-input field-config required magic-tag-enabled" name="{{_name}}[header]">{{#if header}}{{header}}{{else}}<?php _e("<html>\r\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\r\n<body>", 'cf-updraft'); ?>{{/if}}</textarea>
+    </div>
+</div>
+
+<div class="caldera-config-group">
+    <label><?php echo __('Footer', 'cf-updraft'); ?> </label>
+    <div class="caldera-config-field">
+        <textarea class="block-input field-config required magic-tag-enabled" name="{{_name}}[footer]">{{#if footer}}{{footer}}{{else}}<?php _e("</body>\r\n</html>", 'cf-updraft'); ?>{{/if}}</textarea>
+    </div>
 </div>
